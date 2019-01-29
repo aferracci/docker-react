380430dbb087b2469a8a32dbd78c6478f5be12de
FERRACCI Andrea 505335 <andrea.ferracci@bnlmail.com>
2019-01-29 23:27:31 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Ciao a tutti!!
           </a>
         </header>
       </div>
